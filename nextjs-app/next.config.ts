e25c74281fcb5824ff596ddda9c14b98513e30b1
NguyenQuocThinh13 <thinh@saigon.digital>
2025-07-12 14:52:48 +0700

    remove next mjml

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
-import {withMJML} from "next-mjml"
 
-const nextConfig: NextConfig = withMJML({})({
+const nextConfig: NextConfig = {
   env: {
     // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
     SC_DISABLE_SPEEDY: "false",
@@ -16,6 +15,6 @@ const nextConfig: NextConfig = withMJML({})({
       },
     ],
   },
-})
+};
 
 export default nextConfig;
